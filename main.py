e95ec0acfaab8743672c94d943a9b535cf25e741
wbondar <147700834+wbondar@users.noreply.github.com>
2025-03-09 20:46:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from telegram import Update #///
+from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
-import os  # Добавляем импорт модуля os
+import os
+from dotenv import load_dotenv  # Нужно для локальной разработки, на Vercel не используется
 
+load_dotenv() # Загружаем переменные из .env (только для локальной разработки)
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Привет! Я бот.")
@@ -11,13 +13,13 @@ async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 if __name__ == '__main__':
     # Загружаем токен из переменной окружения
-    bot_token = os.environ.get("YOUR_BOT_TOKEN")
+    bot_token = os.environ.get("TELEGRAM_BOT_TOKEN")
 
     if bot_token is None:
         print("Ошибка: Токен бота не найден в переменных окружения!")
-        exit(1)  # Завершаем выполнение, если токена нет
+        exit(1)
 
-    application = ApplicationBuilder().token(bot_token).build()  # Используем токен из переменной
+    application = ApplicationBuilder().token(bot_token).build()
 
     start_handler = CommandHandler('start', start)
     echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
@@ -25,4 +27,5 @@ if __name__ == '__main__':
     application.add_handler(start_handler)
     application.add_handler(echo_handler)
 
-    print("Bot started. Webhook should be set up.")
+    print("Bot started. Webhook should be set up.") # Сообщение для отладки
+    # !!!  application.run_polling() ЗДЕСЬ НЕ НУЖЕН  !!!
